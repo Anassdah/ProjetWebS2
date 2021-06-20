612e1229bfcf4b54e4344446bc4c93990dcd245b
PC PRO ULTRA RAPIDE <anassdah5@gmail.com>
2021-06-20 13:38:50 +0100

    geography commit

---
+++ Ppp2021/js/geographyquestions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is smallest country in the world?",
+    answer: "Vatican",
+    options: [
+      "Belize",
+      "Andorra",
+      "Vatican",
+      "Cyprus"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the most populated city in Africa?",
+    answer: "Lagos",
+    options: [
+      "Lagos",
+      "Cairo",
+      "Casablanca",
+      "Johannesburg"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the country that have its map on its flag?",
+    answer: "Cyprus",
+    options: [
+      "Bermuda",
+      "Tonga",
+      "Tasmania",
+      "Cyprus"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which one of these lakes is largest in Africa",
+    answer: "Lake Victoria",
+    options: [
+      "Lake Tchad ",
+      "Lake Victoria",
+      "Lake Malawi",
+      "Lake Assal"
+    ]
+  },
+    {
+    numb: 5,
+    question: "We love to visit them, but what’s the correct definition of an archipelago?",
+    answer: "A cluster or group of many islands",
+    options: [
+      "A section of land almost fully surrounded by water",
+      "A small, low elevation island on a coral reef",
+      "A large headland extending into a body of water",
+      "A cluster or group of many islands"
+    ]
+  },
+  {
+    numb: 6,
+    question: " how many countries sit along the equator?",
+    answer: "13",
+    options: [
+      "9",
+      "13",
+      "15",
+      "10"
+    ]
+  },
+  {
+    numb: 7,
+    question: " Which country’s territory is geographically closest to the North Pole?",
+    answer: "Canada",
+    options: [
+      "Canada",
+      "iceland",
+      "Norway",
+      "Russia"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Fossils can be found all over the world, but can you guess the age of the oldest fossil ever found?",
+    answer: "Over 3.5 billion years old",
+    options: [
+      "1 billion years old",
+      "300 million years old",
+      "900 million years old",
+      "Over 3.5 billion years old"
+    ]
+  },
+  {
+    numb: 9,
+    question: " In Iceland, you can walk across two tectonic plates, side by side. Which ones, exactly?",
+    answer: "Eurasian and North American",
+    options: [
+      "Eurasian and Antarctic",
+      "Eurasian and North American",
+      "Eurasian and South American",
+      "Eurasian and Indo-Australian"
+    ]
+  },
+  {
+    numb: 10,
+    question: "UNESCO protects the world’s most important sites. How many World Heritage Sites have they inscribed?",
+    answer: "1121",
+    options: [
+      "2345",
+      "1121",
+      "1888",
+      "951"
+    ]
+  },
+  
+];
