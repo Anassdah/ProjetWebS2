687d9cd01c36045d8b2a2e50e9bbf4b04c3fe896
ibrahimainhajar1 <ibrahimainhajar1@gmail.com>
2021-06-22 23:12:11 +0100

    adding history and morocco quizes

---
+++ Ppp2021/js/historyquestions.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+    numb: 1,
+    question: "In 1937 which American aviator disappeared while attempting to circumnavigate the globe in flight?",
+    answer: "Amelia Earhart",
+    options: [
+      "Amelia Earhart",
+      "Bessie Coleman",
+      "Howard Hughes",
+      "Jacqueline Cochran"
+    ]
+  },
+
+  {
+    numb:2 ,
+    question: "Which American actress was found dead at age 36 as a result of barbiturate poisoning, ostensibly self-administered?",
+    answer: "Marilyn Monroe",
+    options: [
+      "Judy Garland",
+      "Carole Lombard",
+      "Grace Kelly",
+      "Marilyn Monroe"
+    ]
+  },
+  {
+  numb: 3,
+    question: "Which controversial Russian figure’s assassins attempted to murder him with a combination of poison, gunshots, and drowning?",
+    answer: "Grigori Rasputin",
+    options: [
+      "Joseph Stalin",
+      "Vladimir Lenin",
+      "Nicholas II",
+      "Grigori Rasputin"
+    ]
+  },
+  {
+    numb:4 ,
+      question: "Which African American explorer was the first known human to set foot on the North Pole?",
+      answer: "Matthew Alexander Henson",
+      options: [
+        "Bart Cubbins, Jr.",
+        "Paulson Quintain",
+        "Nathaniel J. Moore",
+        "Matthew Alexander Henson"
+      ]
+    },
+    {
+        numb: 5,
+          question: "What is the name for a central defensively oriented district in ancient Greek cities?",
+          answer: "acropolis",
+          options: [
+            "temple",
+            "acropolis",
+            "necropolis",
+            "Parthenon"
+          ]
+        },
+        {
+  numb: 6,
+    question: "Which was the most widespread and historically significant writing system in the ancient Middle East?",
+    answer: "cuneiform",
+    options: [
+      "hieroglyphics",
+      "papyrus",
+      "cuneiform",
+      "logograms"
+    ]
+  },
+  {
+    numb:7 ,
+      question: "Which was the most decorated dog in World War II, winning the Distinguished Service Cross, Silver Star, and Purple Heart?",
+      answer: "Chips",
+      options: [
+        "Gunner",
+        "Waxy",
+        "Bullets",
+        "Chips"
+      ]
+    },
+    {
+        numb:8 ,
+          question: "In what year did the Western Roman Empire come to an end?",
+          answer: "476 CE",
+          options: [
+            "476 CE",
+            "517 CE",
+            "1,500 CE",
+            "205 CE"
+          ]
+        },
+        {
+            numb:9 ,
+              question: "Who was a legendary founder of Rome?",
+              answer: "Romulus",
+              options: [
+                "Julius Caesar",
+                "Claudius",
+                "Romulus",
+                "Hannibal"
+              ]
+            },
+            {
+                numb:10 ,
+                  question: "Which one of these was not implicated in the assassination of Julius Caesar?",
+                  answer: "Nero",
+                  options: [
+                    "Nero",
+                    "Cassius",
+                    "Marc Antony",
+                    "Brutus"
+                  ]
+                },
+                
+
+];
---
+++ Ppp2021/js/moroccoquestions.js
@@ -0,0 +1,122 @@
+let questions = [
+    {
+        numb:1 ,
+        question: " Which is the capital of Morocco?",
+        answer: "Rabat",
+        options: [
+          "Tripoli",
+          "Rabat",
+          "Khartoum",
+          "Addis Ababa"
+        ]
+      },
+      {
+        numb:2,
+        question: " Which territory under Morocco is disputed?",
+        answer: " Western Sahara",
+        options: [
+          "Abkhazia",
+          "Somaliland",
+          "South Ossetia",
+          "Western Sahara"
+        ]
+      },
+      {
+        numb: 3,
+        question: "Which famous film has the name of a city of Morocco? ",
+        answer: "Casablanca",
+        options: [
+          "Marrakesh",
+          "Casablanca",
+          "Fes",
+          "Tangier"
+        ]
+      },
+      {
+        numb:4 ,
+        question: "When did France and Spain divide Morocco into their zones of influence? ",
+        answer: "1904",
+        options: [
+          "1912",
+          "1956",
+          "1904",
+          "1900"
+        ]
+      },
+
+      {
+        numb: 5,
+        question: "Which country is to the east of Morocco? ",
+        answer: "Algeria",
+        options: [
+          "Algeria",
+          "Angola",
+          "Ghana",
+          "Ivory Coast"
+        ]
+      },
+
+      {
+        numb: 6,
+        question: "Which is the currency of Morocco? ",
+        answer: "Dirham",
+        options: [ 
+        
+          "Dinar",
+          "Riyal",
+          "Dirham",
+          "Rupee"
+        ]
+      },
+
+      {
+        numb:7 ,
+        question: "Which is the official language of Morocco? ",
+        answer: "Arabic",
+        options: [
+            
+          "Arabic",
+          "Persian",
+          "Turkish",
+          "Dari"
+        ]
+      },
+      {
+        numb:8 ,
+        question: "Who became the King of Morocco in 1999? ",
+        answer: "Mohammed VI",
+        options: [
+            
+          "Mohammed VI",
+          "Mohammed III",
+          "Hassan I",
+          "Hassan II"
+        ]
+      },
+      {
+        numb:9 ,
+        question: " What is in the centre of Morocco’s national flag?",
+        answer: "Five pointed green star",
+        options: [ 
+            
+          "Red star",
+          "Sun",
+          "Wheel",
+          "Five pointed green star"
+        ]
+      },
+      {
+        numb:10 ,
+        question: " What is the date of Marrakesh Declaration? ",
+        answer: "15 April 1994",
+        options: [
+            
+          "20 September 1986",
+          "15 April 1994",
+          "1 January 1999",
+          "1 January 2002"
+        ]
+      },
+
+
+    ];
